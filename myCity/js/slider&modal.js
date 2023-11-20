80d6e146e13e2c98e53b0398f61aaa50048b0bdc
Jenya <113167825+Genya10@users.noreply.github.com>
2023-11-20 11:56:25 +0200

    add bottons in html and logic in js

--- myCity/js/slider&modal.js
+++ myCity/js/slider&modal.js
@@ -1,2 +1,19 @@
+let slideToShow = 0;
+
+const modalBtns = document.querySelectorAll('.modal-btn');
+
+modalBtns.forEach(function(button){
+    button.addEventListener('click',function(){
+    const slideNumberToShow = button.getAttribute('data-index');
+    slideToShow = Number(slideNumberToShow);
+    })
+
+})
+
 //Запуск библиотеки модального окна
 MicroModal.init();
+
+//Запуск библиотеки слайдера
+const sliderContainer = document.querySelector('.splide');
+
+new Splide(sliderContainer).mount();
